53729908683fbe05afa3ffc7fee23223d6515e9f
jf dh <jf@opencollective.brussels>
2021-11-11 12:34:33 +0100

    validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates_uniqueness_of :movie_id, :scope => :list_id
+  validates :comment, length: {minimum: 6}
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,5 @@ class Movie < ApplicationRecord
 
   validates :title, uniqueness: true
   validates :title, presence: true
+  validates :overview, presence: true
 end
