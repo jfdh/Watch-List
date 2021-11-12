f3c85d25527591ca6d36f03aeb1a12ad78001b60
jf dh <jf@opencollective.brussels>
2021-11-12 12:13:12 +0100

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
+  root to: "lists#index"
   resources :lists, only: [ :index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
